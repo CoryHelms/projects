61bca9770722ca59344ae5d78198a60f104fe5c6
corhelms <64820148+CoryHelms@users.noreply.github.com>
2021-04-28 16:37:08 -0400

    adding js for match table

--- scoreboard_app/js/main.js
+++ scoreboard_app/js/main.js
@@ -7,6 +7,49 @@ var awayTeamName = document.querySelector("#away_name");
 var lastMatchGoals = document.querySelector("#score");
 var matchTable = document.querySelector("#match_table");
 
+var matchTable = document.querySelector("#match_table");
+
+//function for building match tiles and UI
+function addMatchTile(){
+    //create match tile div
+    var matchFile = document.createElement('div');
+    addMatchTile.classList.add("match-tile"); //adds class to div tag
+
+    //box for home team
+    var homeTeam = document.createElement('div');
+    homeTeam.classList.add("team");
+
+    //box for home team logo and name
+    var homeTileLogo = document.createElement('img');
+    var homeTileName = document.createElement('p');
+    homeTileLogo.src = data['teams']['home']['logo'];
+    homeTileName.innerHTML = data['teams']['home']['name'];
+
+    //box for away team
+    var awayTeam = document.createElement('div');
+    awayTeam.classList.add("team");
+
+    //box for away team logo and name
+    var awayTileLogo = document.createElement('img');
+    var awayTileName = document.createElement('p');
+    awayTileLogo.src = data['teams']['away']['logo'];
+    awayTileName.innerHTML = data['teams']['away']['name'];
+
+    homeTeam.appendChild(homeTeamLogo);
+    homeTeam.appendChild(homeTeamName);
+
+    awayTeam.appendChild(awayTeamLogo);
+    awayTeam.appendChild(awayTeamLogo);
+
+    var score = document.createElement('p');
+    score.innerHTML = data['goals']['home'] + "  :  " + data['goals']['away'];
+
+    //append all elements to the match tiles
+    MatchTile.appendChild(homeTeam);
+    MatchTile.appendChild(score);
+    MatchTile.appendChild(awayTeam);
+}
+
 //fetching function
 function getData(){
     //connect to API
