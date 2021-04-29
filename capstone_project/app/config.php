737b37aaf159c47c9e66b927b48ca1ba6fe07100
corhelms <64820148+CoryHelms@users.noreply.github.com>
2021-04-29 17:24:11 -0400

    removing unnecessary files

--- capstone_project/app/config.php
+++ capstone_project/app/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$server     = 'db.sice.indiana.edu';
-$username   = 'i494f20_team55';
-$password   = 'my+sql=i494f20_team55';
-$database   = 'i494f20_team55';
+$server     = '#';
+$username   = '#';
+$password   = '#';
+$database   = '#';
 
 $dsn        = "mysql:host=$server;dbname=$database";
 
