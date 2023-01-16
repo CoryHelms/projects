5407f7e91be5be9876406454320cf6f5d6dc814c
Cory Helms <64820148+CoryHelms@users.noreply.github.com>
2023-01-16 13:15:01 -0500

    Create clubSelector.py

---
+++ fm23/clubSelector.py
@@ -0,0 +1,13 @@
+import random
+
+continents = ["Africa", "Asia", "Australia", "Europe", "North America", "South America"]
+
+countries = ["Mexico", "USA", "Canada"]
+
+leagues = ["Liga BBVA MX", "Liga BBVA Expansion MX"]
+
+clubs = ["Atlante", "Atletico La Paz", "Atletico Morelia", "Cancun", "Celaya", "Durango", " Pumas Tabasco", "Oaxaca", "Raya2", "Sinaloa", "Sonora", "Taptio", "Tepatitlan", "Tlaxcala", "UAT", "U. de G.", "Venados", "Zacatecas"]
+
+selection = random.choice(clubs)
+
+print(selection)
