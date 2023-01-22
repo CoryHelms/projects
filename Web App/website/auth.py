93972e67a532cd1290a9bdede57e83f052a33456
Cory Helms <64820148+CoryHelms@users.noreply.github.com>
2023-01-22 17:45:10 -0500

    adding unsaved changes

--- Web App/website/auth.py
+++ Web App/website/auth.py
@@ -27,6 +27,7 @@ def login():
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
