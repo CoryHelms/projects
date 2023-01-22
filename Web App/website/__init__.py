bc3726c38b6931129999e8ec4728ebab131cd584
Cory Helms <64820148+CoryHelms@users.noreply.github.com>
2023-01-22 17:04:54 -0500

    errors fixed.

--- Web App/website/__init__.py
+++ Web App/website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def createApp():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dnslkfjadfas skldjfliwie'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     #adding blueprints
     from .views import views
@@ -12,4 +20,10 @@ def createApp():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+
     return app
+
